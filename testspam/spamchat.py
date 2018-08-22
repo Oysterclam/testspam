dda5440b3617de6eaf7198ec0cc9e84a584cb0c0
Oysterclam <35478562+Oysterclam@users.noreply.github.com>
2018-08-21 22:47:26 -0700

    Update spamchat.py

--- testspam/spamchat.py
+++ testspam/spamchat.py
@@ -14,25 +14,24 @@ driver.find_element_by_xpath("//*[@id='email']").send_keys(email)
 driver.find_element_by_xpath("//*[@id='pass']").send_keys(password)
 driver.find_element_by_xpath("//*[@id='loginbutton']").click()
 
-def createChat(name):
-	for a in range(3):
+def createChat(times):
+	for a in range(times):
 		driver.find_element_by_xpath('//*[@id="u_0_0"]/div/div/div[1]/div[1]/a').click()
-		driver.find_elements_by_class_name('_58al')[1].send_keys('Andy J Yang') #add member
-		time.sleep(0.5)
+		driver.find_elements_by_class_name('_58al')[1].send_keys('Andy J Yang')
+		time.sleep(0.75)
 		driver.find_elements_by_class_name('_58al')[1].send_keys(Keys.RETURN)
 		driver.find_elements_by_class_name('_58al')[1].send_keys('Oscar Chou')
-		time.sleep(0.5)
+		time.sleep(0.75)
 		driver.find_elements_by_class_name('_58al')[1].send_keys(Keys.RETURN)
 		driver.find_element_by_class_name('notranslate').send_keys('create_chat')
 		driver.find_element_by_class_name('notranslate').send_keys(Keys.RETURN)
-		time.sleep(0.5)
-
+		time.sleep(1)
 		driver.find_element_by_class_name('_2jnv').click()
 		time.sleep(0.25)
-		driver.find_elements_by_class_name('notranslate')[0].send_keys(name) #chat name
+		driver.find_elements_by_class_name('notranslate')[0].send_keys('000'+str(a))
 		driver.find_elements_by_class_name('notranslate')[0].send_keys(Keys.RETURN)
 		time.sleep(2)
 
 	# driver.find_elements_by_class_name('')
-createChat('hello')
+createChat(5)
 print('succeed')
